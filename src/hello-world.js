0c9bf2e550464719679b649238c91d55da58419b
Natalie Brickley <natt_rose@yahoo.com>
2019-11-14 12:39:28 -0800

    initial commit

---
+++ src/hello-world.js
@@ -0,0 +1,10 @@
+window.addEventListener('WebComponentsReady', () => {
+  class HelloWorld extends HTMLElement {
+    constructor() {
+      super();
+      const shadowRootEl = this.attachShadow({mode: 'open'});
+      shadowRootEl.innerHTML = '<div>Hello World</div>';
+    }
+  }
+  window.customElements.define('hello-world', HelloWorld);
+});
